12636694069cce7e0a973f854a55eeb5559d647a
Tom Mladenov <tommy.mladenov@gmail.com>
2019-10-05 06:41:02 +0300

    add firebase configs

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,89 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+final dummySnapshot = [
+ {"name": "Filip", "votes": 15},
+ {"name": "Abraham", "votes": 14},
+ {"name": "Richard", "votes": 11},
+ {"name": "Ike", "votes": 10},
+ {"name": "Justin", "votes": 1},
+];
+
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Baby Names',
+     home: MyHomePage(),
+   );
+ }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+ @override
+ _MyHomePageState createState() {
+   return _MyHomePageState();
+ }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+ @override
+ Widget build(BuildContext context) {
+   return Scaffold(
+     appBar: AppBar(title: Text('Baby Name Votes')),
+     body: _buildBody(context),
+   );
+ }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+ Widget _buildBody(BuildContext context) {
+   // TODO: get actual snapshot from Cloud Firestore
+   return _buildList(context, dummySnapshot);
+ }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
-    );
-  }
+ Widget _buildList(BuildContext context, List<Map> snapshot) {
+   return ListView(
+     padding: const EdgeInsets.only(top: 20.0),
+     children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+   );
+ }
+
+ Widget _buildListItem(BuildContext context, Map data) {
+   final record = Record.fromMap(data);
+
+   return Padding(
+     key: ValueKey(record.name),
+     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+     child: Container(
+       decoration: BoxDecoration(
+         border: Border.all(color: Colors.grey),
+         borderRadius: BorderRadius.circular(5.0),
+       ),
+       child: ListTile(
+         title: Text(record.name),
+         trailing: Text(record.votes.toString()),
+         onTap: () => print(record),
+       ),
+     ),
+   );
+ }
+}
+
+class Record {
+ final String name;
+ final int votes;
+ final DocumentReference reference;
+
+ Record.fromMap(Map<String, dynamic> map, {this.reference})
+     : assert(map['name'] != null),
+       assert(map['votes'] != null),
+       name = map['name'],
+       votes = map['votes'];
+
+ Record.fromSnapshot(DocumentSnapshot snapshot)
+     : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+ @override
+ String toString() => "Record<$name:$votes>";
 }
